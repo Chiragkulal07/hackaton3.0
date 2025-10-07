527db40cd2a4d40d102dfad3722f4b6f0a9c48db
Chiragkulal07 <chiragkulal877@gmail.com>
2025-10-07 18:58:52 +0530

    backend setup

---
+++ backend/nittebackend/feedback/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Default home page view
+def home(request):
+    return HttpResponse("Welcome to the Career Readiness Assistant feedback Page!")
